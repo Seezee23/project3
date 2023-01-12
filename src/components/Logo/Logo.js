8961d726ec39d3cd4845895b3084f83b24ea9482
Seezee23 <seezee146@yahoo.com>
2023-01-11 21:40:49 -0500

    update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import styles from './Logo.module.scss';
 export default function Logo() {
 return (
   <div className={styles.Logo}>
-    <div>Welcome</div>
+    <div>Drinks</div>
     </div>
 );
 }
